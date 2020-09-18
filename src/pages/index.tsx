368fdc1d6b1f67a8891106a4ed5980d99260e5ac
Reinoldo <reinoldo@outlook.com>
2020-09-18 19:48:15 +0100

    global style add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
-export default function Home({ title = 'react avancado' }) {
-  return (
-    <div className="container">
-      <Head>
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div>{title}</div>
-
-      <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-    </div>
-  )
+export default function Home() {
+  return <Main />
 }
