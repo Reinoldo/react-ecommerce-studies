528b3960708cb2f27eac90c2a21cdc23b5b42434
Reinoldo <reinoldo@outlook.com>
2020-10-09 17:30:19 +0100

    fixing test configuration and styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,19 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
-)
+import * as S from './styles'
 
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="img/logo.svg"
+      alt="Um atomo em formato de hexagono com o texto React avançado do lado"
+    />
+    <S.Title>React Avancado</S.Title>
+    <S.Description>
+      Bem vindo ao curso de React, NextJS, Typescript
+    </S.Description>
+    <S.Illustration
+      src="img/hero-illustration.svg"
+      alt="um desenvolvedor olhando para 3 monitores com codigo"
+    />
+  </S.Wrapper>
+)
 export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Logo = styled.img`
+  margin-bottom: 2rem;
+  width: 25rem;
+`
+
+export const Title = styled.h1``
+
+export const Description = styled.h2`
+  font-weight: 400;
+  font-size: 2rem;
+`
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`
